2de79bf7f1241b781c41ad4e3d54b7a1f82b6b15
TatjanaZ <G00318193@gmit.ie>
2017-03-28 10:41:23 +0100

    WebQuiz1

---
+++ js/quiz1.js
@@ -0,0 +1,13 @@
+
+
+
+var questions = [
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
+	['What is 34 multiplied by 2?', 36, 68, 66, "B"],
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
+	['What is 99 divided by 3?', 30, 33, 36, "B"],
